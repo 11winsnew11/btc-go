e37f13173ccb289391c61e11438bdb5058cba5ff
whyus <whyusmamet@gmail.com>
2026-03-13 18:32:35 +0000

    f

--- random/hybrid.go
+++ random/hybrid.go
@@ -10,7 +10,7 @@ type Hybrid struct {
 func NewHybrid(seed uint32) *Hybrid {
     return &Hybrid{
         rng:    New(seed),
-        seqPos: 0xe0, 
+        seqPos: 0xf0, 
     }
 }
 
@@ -19,8 +19,8 @@ func (h *Hybrid) Gen2DigitHex() string {
 
     h.seqPos++
     
-    if h.seqPos > 0xef {
-        h.seqPos = 0xe0
+    if h.seqPos >= 0xff {
+        h.seqPos = 0xf0
     }
 
     return fmt.Sprintf("%02x", current)
